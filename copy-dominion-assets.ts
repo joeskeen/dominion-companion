3d09de5455ca983781c32a0423b0dba31fc14b7c
Joe Skeen <joe.skeen@healthcatalyst.com>
2018-06-27 05:50:57 -0600

    add script to copy dominion assets from dominiontabs submodule

---
+++ copy-dominion-assets.ts
@@ -0,0 +1,9 @@
+import { copySync } from 'fs-extra';
+import { join } from 'path';
+
+const dataSrc = join(__dirname, 'dominiontabs/domdiv/card_db');
+const imgSrc = join(__dirname, 'dominiontabs/domdiv/images');
+const dest = join(__dirname, 'src/assets');
+
+copySync(dataSrc, join(dest, 'data'), { recursive: true, overwrite: true });
+copySync(imgSrc, join(dest, 'images'), { recursive: true, overwrite: true });
