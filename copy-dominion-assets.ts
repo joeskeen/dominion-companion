cd7915c965af56e0186ea8b1d2d6580895ec6f00
Joe Skeen <joeskeen@outlook.com>
2019-08-25 13:58:11 -0600

    update dominiontabs and Angular

--- copy-dominion-assets.ts
+++ copy-dominion-assets.ts
@@ -1,8 +1,8 @@
 import { copySync } from 'fs-extra';
 import { join } from 'path';
 
-const dataSrc = join(__dirname, 'dominiontabs/domdiv/card_db');
-const imgSrc = join(__dirname, 'dominiontabs/domdiv/images');
+const dataSrc = join(__dirname, 'dominiontabs/src/domdiv/card_db');
+const imgSrc = join(__dirname, 'dominiontabs/src/domdiv/images');
 const dest = join(__dirname, 'src/assets');
 
 copySync(dataSrc, join(dest, 'data'), { recursive: true, overwrite: true });
