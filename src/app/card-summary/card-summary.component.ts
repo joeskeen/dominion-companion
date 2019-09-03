865c5f6fe74b50cea4d2834c1b925527f2683fef
Joe Skeen <joeskeen@outlook.com>
2019-09-03 12:04:54 -0600

    fix build error

--- src/app/card-summary/card-summary.component.ts
+++ src/app/card-summary/card-summary.component.ts
@@ -1,15 +1,15 @@
 import { Component, Input } from '@angular/core';
-import { Card } from '../models/card';
+import { CompleteCard } from '../models/card';
 import { LocaleType } from '../models/type';
 import { Expansion } from '../models/expansion';
 
 @Component({
-    selector: 'app-card-summary',
-    templateUrl: 'card-summary.component.html',
-    styleUrls: [ 'card-summary.component.scss' ]
+  selector: 'app-card-summary',
+  templateUrl: 'card-summary.component.html',
+  styleUrls: ['card-summary.component.scss']
 })
 export class CardSummaryComponent {
-    @Input() card: Card;
-    @Input() expansion: Expansion;
-    @Input() type: LocaleType;
+  @Input() card: CompleteCard;
+  @Input() expansion: Expansion;
+  @Input() type: LocaleType;
 }
